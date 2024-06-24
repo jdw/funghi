04e91528a4d895315c523a10530dbaabd69f07f7
Johannes Alexis Wirde <johannes.wirde@gmail.com>
2024-06-24 10:44:23 +0200

    Adding enum test data.

---
+++ src/commonTest/kotlin/ParserTestEnum.kt
@@ -0,0 +1,16 @@
+import com.github.jdw.funghi.Funghi
+import kotlin.test.Test
+
+//class ParserTestEnum {
+//	@Test
+//	fun `Should manage to use IdlModel toString for testing enum file no 1`() {
+//		val fileContent = this::class.java.classLoader.getResource("input-enum-01.idl")?.readText()!!
+//		val model = Funghi().parse(fileContent, "input-enum-01.idl")
+//
+//		assert(fileContent == model.toString()) {
+//			println(fileContent)
+//			println("---")
+//			println(model.toString())
+//		}
+//	}
+//}
