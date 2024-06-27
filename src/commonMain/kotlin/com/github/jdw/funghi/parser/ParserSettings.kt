4065f18bb542d848c62f22ada2c8a9069da1ae66
Johannes Alexis Wirde <johannes.wirde@gmail.com>
2024-06-27 02:27:57 +0200

    Parser settings rework.

--- src/commonMain/kotlin/com/github/jdw/funghi/parser/ParserSettings.kt
+++ src/commonMain/kotlin/com/github/jdw/funghi/parser/ParserSettings.kt
@@ -1,103 +1,41 @@
 package com.github.jdw.funghi.parser
 
 enum class ParserSettings {
-	V_20240610 {
-		override fun interfaceKeywords(): Set<String> = setOf("mixin")
-		override fun dictionaryKeywords(): Set<String> = setOf()
-		override fun typedefKeywords(): Set<String> = setOf()
-		override fun modelKeywords(): Set<String> = setOf("typedef", "interface", "dictionary", "enum")
-		override fun integerTypesKeywords(): Set<String> = setOf("byte", "octet", "short", "unsigned short", "long", "unsigned long", "long long", "unsigned long long")
-		override fun numericTypesKeywords(): Set<String> = setOf(*integerTypesKeywords().toTypedArray(), "float", "unrestricted float", "double", "unrestricted double")
-		override fun primitiveTypesKeywords(): Set<String> = setOf(*numericTypesKeywords().toTypedArray(), "bigint", "boolean")
-		override fun stringTypesKeywords(): Set<String> = setOf("DOMString", "ByteString", "USVString")
-		override fun bufferTypesKeywords(): Set<String> = setOf("ArrayBuffer", "SharedArrayBuffer")
-		override fun typedArrayTypesKeywords(): Set<String> = setOf("Int8Array", "Int16Array", "Int32Array", "Uint8Array", "Uint16Array", "Uint32Array", "Uint8ClampedArray", "BigInt64Array", "BigUint64Array", "Float16Array", "Float32Array", "Float64Array")
-		override fun bufferViewTypesKeywords(): Set<String> = setOf(*typedArrayTypesKeywords().toTypedArray(), "DataView")
-		override fun bufferSourceTypesKeywords(): Set<String> = setOf(*bufferTypesKeywords().toTypedArray(), *bufferViewTypesKeywords().toTypedArray())
-		override fun allPredefinedTypesKeywords(): Set<String> =
-			setOf(*integerTypesKeywords().toTypedArray(),
-				*numericTypesKeywords().toTypedArray(),
-				*primitiveTypesKeywords().toTypedArray(),
-				*stringTypesKeywords().toTypedArray(),
-				*bufferTypesKeywords().toTypedArray(),
-				*typedArrayTypesKeywords().toTypedArray(),
-				*bufferViewTypesKeywords().toTypedArray(),
-				*bufferSourceTypesKeywords().toTypedArray())
-		override fun argumentNameKeywords(): Set<String> = setOf("async", "attribute", "callback", "const", "constructor", "deleter", "dictionary", "enum", "getter", "includes", "inherit", "interface", "iterable", "maplike", "mixin", "namespace", "partial", "readonly", "required", "setlike", "setter", "static", "stringifier", "typedef", "unrestricted")
-		override fun complexTypesRegex(): Regex = "([a-zA-Z])+([a-zA-Z0-9_])*".toRegex()
-		override fun identifierReservedKeywords(): Set<String> = setOf("constructor", "toString")
-		override fun identifierIsProhibited(name: String): Boolean = identifierReservedKeywords().contains(name) || name.startsWith("_")
-		override fun identifierRegex(): Regex = "([a-zA-Z])+([a-zA-Z0-9_])*".toRegex()
-		override fun nullableMarkers(): Set<String> = setOf("?")
-		override fun arrayMarkers(): Set<String> = setOf("[]")
-		override fun varargMarkers(): Set<String> = setOf("...")
-		override fun operationRegex(): Regex = "[A-Za-z]+[a-zA-Z_0-9]*\\(.*\\);".toRegex()
-		override fun constructorOperationDefinitionKeywords(): Set<String> = setOf("constructor")
-		override fun constructorOperationRegex(): Regex = "constructor\\(.*\\);".toRegex()
-		override fun attributeDefinitionKeywords(): Set<String> = setOf("attribute")
-		override fun extendedAttributesWildcardMarkers(): Set<String> = setOf("*")
-	},
-	V_20240408 {
-		override fun interfaceKeywords(): Set<String> = setOf("mixin")
-		override fun dictionaryKeywords(): Set<String> = setOf()
-		override fun typedefKeywords(): Set<String> = setOf()
-		override fun modelKeywords(): Set<String> = setOf("typedef", "interface", "dictionary", "enum")
-		override fun integerTypesKeywords(): Set<String> = setOf("byte", "octet", "short", "unsigned short", "long", "unsigned long", "long long", "unsigned long long")
-		override fun numericTypesKeywords(): Set<String> = setOf(*integerTypesKeywords().toTypedArray(), "float", "unrestricted float", "double", "unrestricted double")
-		override fun primitiveTypesKeywords(): Set<String> = setOf(*numericTypesKeywords().toTypedArray(), "bigint", "boolean")
-		override fun stringTypesKeywords(): Set<String> = setOf("DOMString", "ByteString", "USVString")
-		override fun bufferTypesKeywords(): Set<String> = setOf("ArrayBuffer", "SharedArrayBuffer")
-		override fun typedArrayTypesKeywords(): Set<String> = setOf("Int8Array", "Int16Array", "Int32Array", "Uint8Array", "Uint16Array", "Uint32Array", "Uint8ClampedArray", "BigInt64Array", "BigUint64Array", "Float16Array", "Float32Array", "Float64Array")
-		override fun bufferViewTypesKeywords(): Set<String> = setOf(*typedArrayTypesKeywords().toTypedArray(), "DataView")
-		override fun bufferSourceTypesKeywords(): Set<String> = setOf(*bufferTypesKeywords().toTypedArray(), *bufferViewTypesKeywords().toTypedArray())
-		override fun allPredefinedTypesKeywords(): Set<String> =
-			setOf(*integerTypesKeywords().toTypedArray(),
-				*numericTypesKeywords().toTypedArray(),
-				*primitiveTypesKeywords().toTypedArray(),
-				*stringTypesKeywords().toTypedArray(),
-				*bufferTypesKeywords().toTypedArray(),
-				*typedArrayTypesKeywords().toTypedArray(),
-				*bufferViewTypesKeywords().toTypedArray(),
-				*bufferSourceTypesKeywords().toTypedArray())
-		override fun argumentNameKeywords(): Set<String> = setOf("async", "attribute", "callback", "const", "constructor", "deleter", "dictionary", "enum", "getter", "includes", "inherit", "interface", "iterable", "maplike", "mixin", "namespace", "partial", "readonly", "required", "setlike", "setter", "static", "stringifier", "typedef", "unrestricted")
-		override fun complexTypesRegex(): Regex = "([a-zA-Z])+([a-zA-Z0-9_])*".toRegex()
-		override fun identifierReservedKeywords(): Set<String> = setOf("constructor", "toString")
-		override fun identifierIsProhibited(name: String): Boolean = identifierReservedKeywords().contains(name) || name.startsWith("_")
-		override fun identifierRegex(): Regex = "([a-zA-Z])+([a-zA-Z0-9_])*".toRegex()
-		override fun nullableMarkers(): Set<String> = setOf("?")
-		override fun arrayMarkers(): Set<String> = setOf("[]")
-		override fun varargMarkers(): Set<String> = setOf("...")
-		override fun operationRegex(): Regex = "[A-Za-z]+[a-zA-Z_0-9]*\\(.*\\);".toRegex()
-		override fun constructorOperationDefinitionKeywords(): Set<String> = setOf("constructor")
-		override fun constructorOperationRegex(): Regex = "constructor\\(.*\\);".toRegex()
-		override fun attributeDefinitionKeywords(): Set<String> = setOf("attribute")
-		override fun extendedAttributesWildcardMarkers(): Set<String> = setOf("*")
-	};
+	V_20240610,
+	V_20240408;
 
-	abstract fun interfaceKeywords(): Set<String>
-	abstract fun dictionaryKeywords(): Set<String>
-	abstract fun typedefKeywords(): Set<String>
-	abstract fun modelKeywords(): Set<String>
-	abstract fun integerTypesKeywords(): Set<String>
-	abstract fun numericTypesKeywords(): Set<String>
-	abstract fun primitiveTypesKeywords(): Set<String>
-	abstract fun stringTypesKeywords(): Set<String>
-	abstract fun bufferTypesKeywords(): Set<String>
-	abstract fun typedArrayTypesKeywords(): Set<String>
-	abstract fun bufferViewTypesKeywords(): Set<String>
-	abstract fun bufferSourceTypesKeywords(): Set<String>
-	abstract fun allPredefinedTypesKeywords(): Set<String>
-	abstract fun argumentNameKeywords(): Set<String>
-	abstract fun complexTypesRegex(): Regex
-	abstract fun identifierReservedKeywords(): Set<String>
-	abstract fun identifierIsProhibited(name: String): Boolean
-	abstract fun identifierRegex(): Regex
-	abstract fun nullableMarkers(): Set<String>
-	abstract fun arrayMarkers(): Set<String>
-	abstract fun varargMarkers(): Set<String>
-	abstract fun operationRegex(): Regex
-	abstract fun constructorOperationDefinitionKeywords(): Set<String>
-	abstract fun constructorOperationRegex(): Regex
-	abstract fun attributeDefinitionKeywords(): Set<String>
-	abstract fun extendedAttributesWildcardMarkers(): Set<String>
+	open fun interfaceKeywords(): Set<String> = setOf("mixin", "partial")
+	open fun dictionaryKeywords(): Set<String> = emptySet()
+	open fun typedefKeywords(): Set<String> = emptySet()
+	open fun modelKeywords(): Set<String> = setOf("typedef", "interface", "dictionary", "enum")
+	open fun integerTypesKeywords(): Set<String> = setOf("byte", "octet", "short", "unsigned short", "long", "unsigned long", "long long", "unsigned long long")
+	open fun numericTypesKeywords(): Set<String> = setOf(*integerTypesKeywords().toTypedArray(), "float", "unrestricted float", "double", "unrestricted double")
+	open fun primitiveTypesKeywords(): Set<String> = setOf(*numericTypesKeywords().toTypedArray(), "bigint", "boolean")
+	open fun stringTypesKeywords(): Set<String> = setOf("DOMString", "ByteString", "USVString")
+	open fun bufferTypesKeywords(): Set<String> = setOf("ArrayBuffer", "SharedArrayBuffer")
+	open fun typedArrayTypesKeywords(): Set<String> = setOf("Int8Array", "Int16Array", "Int32Array", "Uint8Array", "Uint16Array", "Uint32Array", "Uint8ClampedArray", "BigInt64Array", "BigUint64Array", "Float16Array", "Float32Array", "Float64Array")
+	open fun bufferViewTypesKeywords(): Set<String> = setOf(*typedArrayTypesKeywords().toTypedArray(), "DataView")
+	open fun bufferSourceTypesKeywords(): Set<String> = setOf(*bufferTypesKeywords().toTypedArray(), *bufferViewTypesKeywords().toTypedArray())
+	open fun allPredefinedTypesKeywords(): Set<String> =
+		setOf(*integerTypesKeywords().toTypedArray(),
+			*numericTypesKeywords().toTypedArray(),
+			*primitiveTypesKeywords().toTypedArray(),
+			*stringTypesKeywords().toTypedArray(),
+			*bufferTypesKeywords().toTypedArray(),
+			*typedArrayTypesKeywords().toTypedArray(),
+			*bufferViewTypesKeywords().toTypedArray(),
+			*bufferSourceTypesKeywords().toTypedArray())
+	open fun argumentNameKeywords(): Set<String> = setOf("async", "attribute", "callback", "const", "constructor", "deleter", "dictionary", "enum", "getter", "includes", "inherit", "interface", "iterable", "maplike", "mixin", "namespace", "partial", "readonly", "required", "setlike", "setter", "static", "stringifier", "typedef", "unrestricted")
+	open fun complexTypesRegex(): Regex = "([a-zA-Z])+([a-zA-Z0-9_])*".toRegex()
+	open fun identifierReservedKeywords(): Set<String> = setOf("constructor", "toString")
+	open fun identifierIsProhibited(name: String): Boolean = identifierReservedKeywords().contains(name) || name.startsWith("_")
+	open fun identifierRegex(): Regex = "([a-zA-Z])+([a-zA-Z0-9_])*".toRegex()
+	open fun nullableMarkers(): Set<String> = setOf("?")
+	open fun arrayMarkers(): Set<String> = setOf("[]")
+	open fun varargMarkers(): Set<String> = setOf("...")
+	open fun operationRegex(): Regex = "[A-Za-z]+[a-zA-Z_0-9]*\\(.*\\);".toRegex()
+	open fun constructorOperationDefinitionKeywords(): Set<String> = setOf("constructor")
+	open fun constructorOperationRegex(): Regex = "constructor\\(.*\\);".toRegex()
+	open fun attributeDefinitionKeywords(): Set<String> = setOf("attribute")
+	open fun extendedAttributesWildcardMarkers(): Set<String> = setOf("*")
 }
